d8aaacd9203a04f7cf9b81117064ad8906f24c3f
Daniel Castro <daniel.castro@digitalhub.man>
2021-11-22 11:23:21 +0000

    f1 change 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1>1</h1>
         <h1>2</h1>
+        <h1>3</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
